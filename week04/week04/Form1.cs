50f0cf5c21f806942a5b650c83dd4736f1a9f55e
fj6g9w <fj6g9w@SZA0025793>
2022-10-07 12:28:22 +0200

    hatodik

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -17,16 +17,46 @@ namespace week04
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlWs;
         public Form1()
         {
                        
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();   //Excel elindítása és az applikáció objektum betöltése
+                xlWb = xlApp.Workbooks.Add(Missing.Value);   //új munkafüzet
+                xlWs = xlWb.ActiveSheet;   //új munkalap
+                //CreateTable();   //új tábla létrehozása
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;   //control átadása a felhasználónak
+
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                //hiba esetén az excel bezárása automatikusan
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
         }
     }
 }
