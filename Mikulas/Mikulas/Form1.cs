5798322927463c2f63c3ab1505eabfc6205cc476
fj6g9w <fj6g9w@SZA0025793>
2022-11-04 12:53:22 +0100

    nyolcadik

--- Mikulas/Mikulas/Form1.cs
+++ Mikulas/Mikulas/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Mikulas.Entities;
+﻿using Mikulas.Abstraction;
+using Mikulas.Entities;
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
@@ -7,10 +8,10 @@ namespace Mikulas
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
+        private List<Toy> _toys = new List<Toy>();
+        private IToyFactory _factory;
 
-        public BallFactory Factory
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -25,30 +26,30 @@ namespace Mikulas
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var b = Factory.CreateNew();
-            _balls.Add(b);
-            b.Left = b.Width * (-1);
+            var t = Factory.CreateNew();
+            _toys.Add(t);
+            t.Left = t.Width * (-1);
             //_balls.Add(b);
-            mainPanel.Controls.Add(b);
+            mainPanel.Controls.Add(t);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var b in _balls)
+            foreach (var t in _toys)
             {
-                b.MoveToy();
-                if (b.Left > maxPosition)
+                t.MoveToy();
+                if (t.Left > maxPosition)
                 {
-                    maxPosition = b.Left;
+                    maxPosition = t.Left;
                 }
             }
 
             if (maxPosition >= 1000)
             {
-                var oldest = _balls[0];
+                var oldest = _toys[0];
                 mainPanel.Controls.Remove(oldest);
-                _balls.Remove(oldest);
+                _toys.Remove(oldest);
             }
         }
     }
