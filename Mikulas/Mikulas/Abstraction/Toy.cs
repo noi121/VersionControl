fce09bc95a21d35c3c02e827a7026bbe387b31e5
fj6g9w <fj6g9w@SZA0025793>
2022-11-04 12:37:25 +0100

    negyedik

---
+++ Mikulas/Mikulas/Abstraction/Toy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mikulas.Abstraction
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = Height = 50;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected abstract void DrawImage(Graphics g);
+        
+
+        public void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
