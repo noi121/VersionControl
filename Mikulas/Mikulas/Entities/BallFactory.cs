f3de949fbc5351ba292d94d7c6cf3532df8260bf
fj6g9w <fj6g9w@SZA0025793>
2022-11-04 12:12:25 +0100

    harmadik

---
+++ Mikulas/Mikulas/Entities/BallFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikulas.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+
+        public BallFactory()
+        {
+
+        }
+    }
+}
