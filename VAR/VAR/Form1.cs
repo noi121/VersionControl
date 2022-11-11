df21b52be09f0afe0e961e48f2f23baf890cb7b7
fj6g9w <fj6g9w@SZA0025793>
2022-11-11 12:20:21 +0100

    nyolcas

--- VAR/VAR/Form1.cs
+++ VAR/VAR/Form1.cs
@@ -23,6 +23,20 @@ namespace VAR
             Ticks = context.Tick.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervallum = 30;
+            DateTime kezdoDatum = (from x in Ticks select x.TradingDay).Min();
+            DateTime vegDatum = new DateTime(2016, 12, 30);
+            TimeSpan z = vegDatum - kezdoDatum;
+            for (int i = 0; i < z.Days - intervallum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdoDatum.AddDays(i + intervallum)) - GetPortfolioValue(kezdoDatum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+            var nyeresegRendezve = (from x in Nyereségek orderby x select x).ToList();
+            MessageBox.Show(nyeresegRendezve[nyeresegRendezve.Count() / 5].ToString());
         }
 
         private void CreatePortfolio()
